3dfe5cd263891f875e0102c7ef9a6e2944d0b76d
mariana <mariasyad@gmail.com>
2021-06-09 17:20:35 +0300

    adding setup method and division tests into positive and negative ones

--- homeworks/HW5_Tests_Practice/test_game_by_pytest.py
+++ homeworks/HW5_Tests_Practice/test_game_by_pytest.py
@@ -1,24 +1,36 @@
 import pytest
 from game import TicTacToe
 
-game = TicTacToe()
-game.board[4] = "X"
+my_game = TicTacToe()
+
+
+def setup():
+    my_game.board[4] = "X"
+    my_game.board[5] = "0"
+    my_game.board[6] = "X"
 
 
 def test_available_moves():
-    assert 4 not in game.available_moves()
-    assert 3 in game.available_moves()
-    assert 1 in game.available_moves()
-    assert 8 in game.available_moves()
+    assert 4 not in my_game.available_moves()
+    assert 5 not in my_game.available_moves()
+    assert 3 in my_game.available_moves()
+    assert 1 in my_game.available_moves()
+    assert 8 in my_game.available_moves()
+
+
+def test_available_moves_negative():
     with pytest.raises(AssertionError):
-        assert 9 in game.available_moves()
-        assert -1 in game.available_moves()
+        assert 9 in my_game.available_moves()
+        assert -1 in my_game.available_moves()
 
 
 def test_make_move():
-    assert game.make_move(4, "X") is False
-    assert game.make_move(5, "X") is True
+    assert my_game.make_move(4, "X") is False
+    assert my_game.make_move(5, "X") is False
+    assert my_game.make_move(7, "X") is True
+
+
+def test_make_move_negative():
     with pytest.raises(IndexError):
-        assert game.make_move(9, "X") is False
-    assert game.board[5] != " "
-    assert game.board[7] == " "
+        assert my_game.make_move(9, "X") is False
+        assert my_game.make_move(-1, "0") is False
